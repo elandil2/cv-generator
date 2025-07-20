fae4faa3076d7d79e08cb439e212e3cdae396fe8
elandil2 <saficengiz1@gmail.com>
2025-07-21 01:27:26 +0300

    Add Streamlit configuration

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,20 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings:
     def __init__(self):
-        self.groq_api_key = os.getenv("GROQ_API_KEY", "")
+        # Try Streamlit secrets first, then environment variables
+        try:
+            self.groq_api_key = st.secrets["GROQ_API_KEY"]
+        except:
+            self.groq_api_key = os.getenv("GROQ_API_KEY", "")
+        
         self.app_name = "CV & Cover Letter Generator"
         self.version = "1.0.0"
         self.debug = False
-        self.max_file_size = 5 * 1024 * 1024  # 5MB
+        self.max_file_size = 5 * 1024 * 1024
 
 settings = Settings()
